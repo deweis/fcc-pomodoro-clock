6b84b8f606b98daac2b725c9d26a2f0258c80006
Dennis <deweis@gmx.ch>
2018-03-24 09:46:30 +0100

    Setup timer with fixed duration

---
+++ script.js
@@ -0,0 +1,18 @@
+let sessionLength = 1;
+let breakLength = 1;
+
+document.getElementById("sessionLength").innerHTML = sessionLength;
+document.getElementById("breakLength").innerHTML = breakLength;
+
+let timer = sessionLength * 60;
+
+var x = setInterval( function() {
+        if (timer > 0) {
+          document.getElementById("timer").innerHTML = timer;
+          timer -= 1;
+        }
+        else {
+          document.getElementById("timer").innerHTML = "Session over";
+          clearInterval(x);
+        }
+}, 1000);
