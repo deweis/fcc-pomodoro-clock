d1f37f49f16e18de85c1d3ce024f4e10400858c2
Dennis <deweis@gmx.ch>
2018-03-25 12:43:56 +0200

    Add click option to start and stop the timer

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 let sessionLength = 1;
 let breakLength = 1;
+let time = sessionLength * 60;
 
 document.getElementById("sessionLength").innerHTML = sessionLength;
 document.getElementById("breakLength").innerHTML = breakLength;
+document.getElementById("timer").innerHTML = time;
 
-let timer = sessionLength * 60;
+let running = 0;
+let x;
 
-var x = setInterval( function() {
-        if (timer > 0) {
-          document.getElementById("timer").innerHTML = timer;
-          timer -= 1;
-        }
-        else {
-          document.getElementById("timer").innerHTML = "End";
-          clearInterval(x);
-        }
-}, 1000);
+document.getElementById("session").addEventListener("click", function() {
+  if (running === 0) {
+      running = 1;
+      x = setInterval( function() {
+              if (time > 0) {
+                document.getElementById("timer").innerHTML = time;
+                time -= 1;
+              }
+              else {
+                document.getElementById("timer").innerHTML = "End";
+                clearInterval(x);
+              }
+      }, 1000);
+  } else {
+      running = 0;
+      clearInterval(x);
+  }
+});
