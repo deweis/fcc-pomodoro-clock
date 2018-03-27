a3ac126299055f106cc4d65c331d12a1607e3a94
Dennis <deweis@gmx.ch>
2018-03-27 09:36:18 +0200

    Add break counter
    - Now it loops infinitely between session and break until the user
    stops it

--- script.js
+++ script.js
@@ -9,20 +9,34 @@ document.getElementById("timer").innerHTML = sessionLength;
 
 let running = 0;
 let x;
+let isBreak = 0;
 
 document.getElementById("session").addEventListener("click", function() {
   if (running === 0) {
       running = 1;
       x = setInterval( function() {
               timer -= 1;
-              if (timer > 0) {
+              if (timer >= 0) {
+                let mins = Math.floor(timer/60);
+                let secs = timer % 60 > 9 ? timer % 60 : `0${timer % 60}`;
+                document.getElementById("timer").innerHTML = `${mins}:${secs}`;
+                // check for linear-gradient for changing the background color
+              }
+              else if (isBreak === 0) {
+                isBreak = 1;
+                timer = breaker;
+                document.getElementById("sessionTitle").innerHTML = "Break!";
                 let mins = Math.floor(timer/60);
                 let secs = timer % 60 > 9 ? timer % 60 : `0${timer % 60}`;
                 document.getElementById("timer").innerHTML = `${mins}:${secs}`;
               }
               else {
-                document.getElementById("timer").innerHTML = "End";
-                clearInterval(x);
+                isBreak = 0;
+                timer = sessionLength * 60;
+                document.getElementById("sessionTitle").innerHTML = "Session";
+                let mins = Math.floor(timer/60);
+                let secs = timer % 60 > 9 ? timer % 60 : `0${timer % 60}`;
+                document.getElementById("timer").innerHTML = `${mins}:${secs}`;
               }
       }, 1000);
   } else {
