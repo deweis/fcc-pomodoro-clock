58d6617954d6d95f8b1f614d020a2a267306c507
Dennis <deweis@gmx.ch>
2018-04-08 11:59:54 +0200

    Activate start/pause button

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-let sessionLength = 2;
-let breakLength = 1;
+let sessionLength = 0.1;
+let breakLength = 0.1;
+let timer = sessionLength * 60;
+let breaker = breakLength * 60;
+
+/*
+
+
+let duration = sessionLength * 60;
+let total = breaker + timer;
+let share = breaker + timer;
+//console.log(total);*/
+
 
 /**
 * Load initial page
@@ -7,21 +18,114 @@ let breakLength = 1;
 document.getElementById("sessionLength").innerHTML = sessionLength;
 document.getElementById("breakLength").innerHTML = breakLength;
 
-/*
-let sessionLength = 45;
-let breakLength = 1;
-let timer = sessionLength * 60;
-let breaker = breakLength * 60;
-let duration = sessionLength * 60;
-let total = breaker + timer;
-let share = breaker + timer;
-//console.log(total);*/
+/**
+* Increase- / Decrease the session- and break durations
+*/
+document.getElementById("sessionPlus").addEventListener("click", function() {
+  sessionLength += 1;
+  //timer = sessionLength * 60;
+  //duration = sessionLength * 60;
+  //total = breaker + timer;
+  //share = breaker + timer;
+  document.getElementById("sessionLength").innerHTML = sessionLength;
+  //updateTimer();
+  //updateBackground();
+});
+
+document.getElementById("sessionMinus").addEventListener("click", function() {
+  if (sessionLength === 1) {return;}
+  sessionLength -= 1;
+  //timer = sessionLength * 60;
+  //duration = sessionLength * 60;
+  //total = breaker + timer;
+  //share = breaker + timer;
+  document.getElementById("sessionLength").innerHTML = sessionLength;
+  //updateTimer();
+  //updateBackground();
+});
+
+document.getElementById("breakPlus").addEventListener("click", function() {
+  breakLength += 1;
+  //breaker = breakLength * 60;
+  //total = breaker + timer;
+  //share = breaker + timer;
+  document.getElementById("breakLength").innerHTML = breakLength;
+});
+
+document.getElementById("breakMinus").addEventListener("click", function() {
+  if (breakLength === 1) {return;}
+  breakLength -= 1;
+  //breaker = breakLength * 60;
+  //total = breaker + timer;
+  //share = breaker + timer;
+  document.getElementById("breakLength").innerHTML = breakLength;
+});
+
+
+/**
+* Update Timer
+*/
+function updateTimer() {
+  mins = Math.floor(timer/60);
+  secs = timer % 60 > 9 ? timer % 60 : `0${timer % 60}`;
+  //document.getElementById("canvas").style.height = `"${document.getElementById("canvas").clientHeight}"`;
+  document.getElementById("canvas").innerHTML = `${mins}:${secs}`;
+}
+
+
+/**
+* Start and Stop the timer by cklicking the button
+*/
+let running = 0;
+let x;
+let isBreak = 0;
+
+document.getElementById("btn").addEventListener("click", function() {
+  if (running === 0) {
+      running = 1;
+      document.getElementById("btn").innerHTML = "Pause";
+      x = setInterval( function() {
+              timer -= 1;
+              //share -= 1;
+              if (timer >= 0) {
+                if (isBreak === 0) {document.getElementById("title").innerHTML = "Session";}
+                updateTimer();
+                //updateBackground();
+              }
+              else if (isBreak === 0) {
+                isBreak = 1;
+                timer = breaker;
+                //duration = breakLength * 60;
+                //share = breaker;
+                document.getElementById("title").innerHTML = "Break!";
+                //document.getElementById("btn").style.border = "5px solid #f44"
+                //document.getElementById("battery").innerHTML = "&#xf243;";
+                //document.getElementById("battery").style.color = "#ff3d00";
+                updateTimer();
+                //updateBackground();
+              }
+              else {
+                isBreak = 0;
+                timer = sessionLength * 60;
+                //duration = sessionLength * 60;
+                //share = breaker + timer;
+                document.getElementById("title").innerHTML = "Session";
+                //document.getElementById("btn").style.border = "5px solid #9C0";
+                //document.getElementById("battery").innerHTML = "&#xf240;";
+                //document.getElementById("battery").style.color = "#64dd17";
+                updateTimer();
+                //updateBackground();
+              }
+      }, 1000);
+  } else {
+      running = 0;
+      document.getElementById("btn").innerHTML = "Start";
+      clearInterval(x);
+  }
+});
 
 /**
 * Load initial page
-
-document.getElementById("sessionLength").innerHTML = sessionLength;
-document.getElementById("breakLength").innerHTML = breakLength;
 
 let mins = Math.floor(timer/60);
 let secs = timer % 60 > 9 ? timer % 60 : `0${timer % 60}`;
@@ -38,59 +142,8 @@ document.getElementById("battery").innerHTML = "&#xf240;";
 document.getElementById("battery").style.color = "#64dd17";
 document.getElementById("load-state").innerHTML = '100%';
 */
-/**
-* Update Timer
 
-function updateTimer() {
-  mins = Math.floor(timer/60);
-  secs = timer % 60 > 9 ? timer % 60 : `0${timer % 60}`;
-  document.getElementById("canvas").style.height = `"${document.getElementById("canvas").clientHeight}"`;
-  document.getElementById("canvas").innerHTML = `${mins}:${secs}`;
-}
-*/
-/**
-* Increase- / Decrease the session- and break durations
 
-document.getElementById("sessionPlus").addEventListener("click", function() {
-  sessionLength += 1;
-  timer = sessionLength * 60;
-  duration = sessionLength * 60;
-  total = breaker + timer;
-  share = breaker + timer;
-  document.getElementById("sessionLength").innerHTML = sessionLength;
-  updateTimer();
-  updateBackground();
-});
-
-document.getElementById("sessionMinus").addEventListener("click", function() {
-    if (sessionLength === 1) {return;}
-  sessionLength -= 1;
-  timer = sessionLength * 60;
-  duration = sessionLength * 60;
-  total = breaker + timer;
-  share = breaker + timer;
-  document.getElementById("sessionLength").innerHTML = sessionLength;
-  updateTimer();
-  updateBackground();
-});
-
-document.getElementById("breakPlus").addEventListener("click", function() {
-  breakLength += 1;
-  breaker = breakLength * 60;
-  total = breaker + timer;
-  share = breaker + timer;
-  document.getElementById("breakLength").innerHTML = breakLength;
-});
-
-document.getElementById("breakMinus").addEventListener("click", function() {
-  if (breakLength === 1) {return;}
-  breakLength -= 1;
-  breaker = breakLength * 60;
-  total = breaker + timer;
-  share = breaker + timer;
-  document.getElementById("breakLength").innerHTML = breakLength;
-});
-*/
 /**
 * Update Timer background
 
@@ -111,53 +164,4 @@ function updateBackground() {
     document.getElementById("battery").innerHTML = "&#xf244;";
   }
 }
-*/
-/**
-* Start and Stop the timer by cklicking the button
-
-let running = 0;
-let x;
-let isBreak = 0;
-
-document.getElementById("btn").addEventListener("click", function() {
-  if (running === 0) {
-      running = 1;
-      x = setInterval( function() {
-              timer -= 1;
-              share -= 1;
-              if (timer >= 0) {
-                if (isBreak === 0) {document.getElementById("btn").innerHTML = "Session";}
-                updateTimer();
-                updateBackground();
-              }
-              else if (isBreak === 0) {
-                isBreak = 1;
-                timer = breaker;
-                duration = breakLength * 60;
-                share = breaker;
-                document.getElementById("btn").innerHTML = "Break!";
-                document.getElementById("btn").style.border = "5px solid #f44"
-                document.getElementById("battery").innerHTML = "&#xf243;";
-                document.getElementById("battery").style.color = "#ff3d00";
-                updateTimer();
-                updateBackground();
-              }
-              else {
-                isBreak = 0;
-                timer = sessionLength * 60;
-                duration = sessionLength * 60;
-                share = breaker + timer;
-                document.getElementById("btn").innerHTML = "Session";
-                document.getElementById("btn").style.border = "5px solid #9C0";
-                document.getElementById("battery").innerHTML = "&#xf240;";
-                document.getElementById("battery").style.color = "#64dd17";
-                updateTimer();
-                updateBackground();
-              }
-      }, 1000);
-  } else {
-      running = 0;
-      clearInterval(x);
-  }
-});
 */
