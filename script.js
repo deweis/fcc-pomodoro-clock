0c9f51a55c3907603b1760a91fae745f574ef5d5
Dennis <deweis@gmx.ch>
2018-04-09 11:16:56 +0200

    Add dynamic background
    - Changes now according to timer countdown
    - Minor color changes

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-let sessionLength = 0.1;
-let breakLength = 0.1;
+let sessionLength = 2;
+let breakLength = 1;
 let timer = sessionLength * 60;
 let breaker = breakLength * 60;
-
-/*
-
-
 let duration = sessionLength * 60;
-let total = breaker + timer;
-let share = breaker + timer;
-//console.log(total);*/
-
 
 /**
 * Load initial page
@@ -23,10 +15,8 @@ document.getElementById("breakLength").innerHTML = breakLength;
 */
 document.getElementById("sessionPlus").addEventListener("click", function() {
   sessionLength += 1;
-  //timer = sessionLength * 60;
-  //duration = sessionLength * 60;
-  //total = breaker + timer;
-  //share = breaker + timer;
+  timer = sessionLength * 60;
+  duration = sessionLength * 60;
   document.getElementById("sessionLength").innerHTML = sessionLength;
   //updateTimer();
   //updateBackground();
@@ -35,10 +25,8 @@ document.getElementById("sessionPlus").addEventListener("click", function() {
 document.getElementById("sessionMinus").addEventListener("click", function() {
   if (sessionLength === 1) {return;}
   sessionLength -= 1;
-  //timer = sessionLength * 60;
-  //duration = sessionLength * 60;
-  //total = breaker + timer;
-  //share = breaker + timer;
+  timer = sessionLength * 60;
+  duration = sessionLength * 60;
   document.getElementById("sessionLength").innerHTML = sessionLength;
   //updateTimer();
   //updateBackground();
@@ -46,21 +34,16 @@ document.getElementById("sessionMinus").addEventListener("click", function() {
 
 document.getElementById("breakPlus").addEventListener("click", function() {
   breakLength += 1;
-  //breaker = breakLength * 60;
-  //total = breaker + timer;
-  //share = breaker + timer;
+  breaker = breakLength * 60;
   document.getElementById("breakLength").innerHTML = breakLength;
 });
 
 document.getElementById("breakMinus").addEventListener("click", function() {
   if (breakLength === 1) {return;}
   breakLength -= 1;
-  //breaker = breakLength * 60;
-  //total = breaker + timer;
-  //share = breaker + timer;
+  breaker = breakLength * 60;
   document.getElementById("breakLength").innerHTML = breakLength;
 });
-
 
 /**
 * Update Timer
@@ -68,10 +51,17 @@ document.getElementById("breakMinus").addEventListener("click", function() {
 function updateTimer() {
   mins = Math.floor(timer/60);
   secs = timer % 60 > 9 ? timer % 60 : `0${timer % 60}`;
-  //document.getElementById("canvas").style.height = `"${document.getElementById("canvas").clientHeight}"`;
   document.getElementById("canvas").innerHTML = `${mins}:${secs}`;
 }
-
+/**
+* Update Timer background
+*/
+function updateBackground() {
+  let filling = isBreak === 1 ? '#f44' : '#9C0';
+  let rangeFilling = Math.round(Number((1-(timer/(duration))).toFixed(2)) * 100);
+  let rangeGray = 100-rangeFilling;
+  document.getElementById("btn").style.background = "linear-gradient(#fff, #fff "+rangeGray+"%, "+filling+" "+rangeGray+"%, "+filling+")";
+}
 
 /**
 * Start and Stop the timer by cklicking the button
@@ -83,38 +73,46 @@ let isBreak = 0;
 document.getElementById("btn").addEventListener("click", function() {
   if (running === 0) {
       running = 1;
-      document.getElementById("btn").innerHTML = "Pause";
+      //document.getElementById("btn").innerHTML = "Pause";
       x = setInterval( function() {
               timer -= 1;
-              //share -= 1;
               if (timer >= 0) {
-                if (isBreak === 0) {document.getElementById("title").innerHTML = "Session";}
+                if (isBreak === 0) {
+                  document.getElementById("title").innerHTML = "Session";
+                  document.getElementById("btn").innerHTML = "Pause";
+                  document.getElementById("btn").style.border = "2px solid #9C0";
+                  document.getElementById("btn").style.color = "#E1E2E1";
+                  document.getElementById("title").style.color = "#9C0";
+                  document.getElementById("canvas").style.color = "#9C0";
+                }
                 updateTimer();
-                //updateBackground();
+                updateBackground();
               }
               else if (isBreak === 0) {
                 isBreak = 1;
                 timer = breaker;
-                //duration = breakLength * 60;
-                //share = breaker;
+                duration = breakLength * 60;
                 document.getElementById("title").innerHTML = "Break!";
-                //document.getElementById("btn").style.border = "5px solid #f44"
-                //document.getElementById("battery").innerHTML = "&#xf243;";
-                //document.getElementById("battery").style.color = "#ff3d00";
+                document.getElementById("btn").innerHTML = "Pause";
+                document.getElementById("btn").style.border = "2px solid #f44"
+                document.getElementById("btn").style.color = "#E1E2E1";
+                document.getElementById("title").style.color = "#f44";
+                document.getElementById("canvas").style.color = "#f44";
                 updateTimer();
-                //updateBackground();
+                updateBackground();
               }
               else {
                 isBreak = 0;
                 timer = sessionLength * 60;
-                //duration = sessionLength * 60;
-                //share = breaker + timer;
+                duration = sessionLength * 60;
                 document.getElementById("title").innerHTML = "Session";
-                //document.getElementById("btn").style.border = "5px solid #9C0";
-                //document.getElementById("battery").innerHTML = "&#xf240;";
-                //document.getElementById("battery").style.color = "#64dd17";
+                document.getElementById("btn").innerHTML = "Pause";
+                document.getElementById("btn").style.border = "2px solid #9C0";
+                document.getElementById("btn").style.color = "#E1E2E1";
+                document.getElementById("title").style.color = "#9C0";
+                document.getElementById("canvas").style.color = "#9C0";
                 updateTimer();
-                //updateBackground();
+                updateBackground();
               }
       }, 1000);
   } else {
@@ -141,27 +139,4 @@ console.log(window.getComputedStyle(document.getElementById("canvas")).fontSize)
 document.getElementById("battery").innerHTML = "&#xf240;";
 document.getElementById("battery").style.color = "#64dd17";
 document.getElementById("load-state").innerHTML = '100%';
-*/
-
-
-/**
-* Update Timer background
-
-function updateBackground() {
-  let filling = isBreak === 1 ? '#f44' : '#9C0';
-  let rangeFilling = Math.round(Number((1-(timer/(duration))).toFixed(2)) * 100);
-  let rangeGray = 100-rangeFilling;
-  let state = 100-Math.round(Number((1-(share/(total))).toFixed(2)) * 100);
-  document.getElementById("btn").style.background = "linear-gradient(#333, #333 "+rangeGray+"%, "+filling+" "+rangeGray+"%, "+filling+")";
-  document.getElementById("load-state").innerHTML = state+'%';
-  if (rangeGray < 66 && isBreak === 0) {
-    document.getElementById("battery").innerHTML = "&#xf241;";
-  }
-  if (rangeGray < 33 && isBreak === 0) {
-    document.getElementById("battery").innerHTML = "&#xf242;";
-  }
-  if (rangeGray === 0 && isBreak === 1) {
-    document.getElementById("battery").innerHTML = "&#xf244;";
-  }
-}
 */
